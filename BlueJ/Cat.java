036b4f4ced5ddffc7f4ff08532c0de802d97360e
cattythegamer-tcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2021-02-11 12:13:43 +1300

    Started declaring variables in java

---
+++ BlueJ/Cat.java
@@ -0,0 +1,36 @@
+
+/**
+ * Cats have hair colour and type
+ * As defined here they can only bark
+ * 
+ * @author Jayden Everest 
+ * @version 11/02/21
+ */
+public class Cat
+{
+    // fields
+    private String colour;
+    private String hair;
+    private int age;
+
+    /**
+     * Constructor for objects of class Cat
+     */
+    public Cat()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}

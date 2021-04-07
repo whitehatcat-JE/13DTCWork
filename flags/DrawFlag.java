0a0cf77e7b5bfed056defc81d07ba3aad3470bc7
cattythegamer-tcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2021-04-08 11:55:21 +1200

    Added flags

--- flags/DrawFlag.java
+++ flags/DrawFlag.java
@@ -10,20 +10,22 @@ import java.awt.Color;
  * @version 1/04/21
  */
 public class DrawFlag{
-    private final static float WIDTH = 600;
-    private final static float HEIGHT = 300;
+    private final float WIDTH = 600;
+    private final float HEIGHT = 300;
     /**
      * Constructor for objects of class DrawFlag
      */
     public DrawFlag(){
         UI.initialise();
         UI.addButton("Quit", UI::quit);  // Comment out to checkstyle
+
+        hungary();
     }
 
     /**
      * Draws a horizontal flag with given colours
      */
-    private static void drawHorizontalFlag(ArrayList<Color> colors) {
+    private void drawHorizontalFlag(ArrayList<Color> colors) {
         double lineHeight = HEIGHT / colors.size(); // Determines vertical spacing
         for (int i = 0; i < colors.size(); i++) { // Draws each color
             UI.setColor(colors.get(i));
@@ -34,7 +36,7 @@ public class DrawFlag{
     /**
      * Draws a vertical flag with given colors
      */
-    private static void drawVerticalFlag(ArrayList<Color> colors) {
+    private void drawVerticalFlag(ArrayList<Color> colors) {
         double lineWidth = WIDTH / colors.size(); // Determines horizontal spacing
         for (int i = 0; i < colors.size(); i++) { // Draws each color
             UI.setColor(colors.get(i));
@@ -43,14 +45,123 @@ public class DrawFlag{
     }
 
     /**
-     * Sets colors and draws flag
+     * Draws Hungary
      */
-    public static void main(String[] args){
+    public void hungary(){
         ArrayList<Color> flagDesign = new ArrayList<Color>();
         // Adds colors
+        flagDesign.add(new Color(205,42,61));
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(new Color(67,111,76));
+        // Draws flag
+        drawHorizontalFlag(flagDesign);
+    }
+
+    /**
+     * Draws Estonia
+     */
+    public void estonia(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(0,113,205));
         flagDesign.add(Color.BLACK);
-        flagDesign.add(Color.RED);
-        flagDesign.add(Color.YELLOW);
+        flagDesign.add(Color.WHITE);
+        // Draws flag
+        drawHorizontalFlag(flagDesign);
+    }
+
+    /**
+     * Draws Thai
+     */
+    public void thai(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(160, 24, 47));
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(new Color(44, 41, 72));
+        flagDesign.add(new Color(44, 41, 72));
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(new Color(160, 24, 47));
+        // Draws flag
+        drawHorizontalFlag(flagDesign);
+    }
+
+    /**
+     * Draws French
+     */
+    public void french(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(0,34,144));
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(new Color(230,40,55));
+        // Draws flag
+        drawVerticalFlag(flagDesign);
+    }
+
+    /**
+     * Draws Dutch
+     */
+    public void dutch(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(170,26,38));
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(new Color(0,34,144));
+        // Draws flag
+        drawHorizontalFlag(flagDesign);
+    }
+
+    /**
+     * Draws Belgium
+     */
+    public void belgium(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(35,31,32));
+        flagDesign.add(new Color(255,242,0));
+        flagDesign.add(new Color(237,28,36));
+        // Draws flag
+        drawVerticalFlag(flagDesign);
+    }
+
+    /**
+     * Draws Botswana (Don't look too hard at this :))
+     */
+    public void botswana(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(Color.BLACK);
+        flagDesign.add(Color.BLACK);
+        flagDesign.add(Color.BLACK);
+        flagDesign.add(Color.WHITE);
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        flagDesign.add(new Color(0,189,242));
+        // Draws flag
+        drawHorizontalFlag(flagDesign);
+    }
+
+    /**
+     * Draws Colombia
+     */
+    public void colombia(){
+        ArrayList<Color> flagDesign = new ArrayList<Color>();
+        // Adds colors
+        flagDesign.add(new Color(255,242,0));
+        flagDesign.add(new Color(255,242,0));
+        flagDesign.add(new Color(0,102,179));
+        flagDesign.add(new Color(237,28,36));
         // Draws flag
         drawHorizontalFlag(flagDesign);
     }

51ceee5ec2218605d05e9be234aaa966e599beda
Whitehatcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2022-03-04 17:53:16 +1300

    Made small text RPG in c++

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,74 +1,147 @@
-// Number guessing game
+// Text RPG
 #include <iostream>
+#include <array>
 
 using namespace std;
 
-// Global variables
-int a = 0, b = 0, c = 0, addVal = 0, multVal = 0, difficulty = 2;
+bool fight(int playerStartingHealth, int level) {
+	string names[] = {
+		"Slime",
+		"Spider",
+		"Fox",
+		"Tiger",
+		"Wolf",
+		"Snake",
+		"Robot",
+		"T-Rex",
+		"Attack Helicopter",
+		"Slime Prophet",
+		"Slime God"
+	};
+	// Attack damage, health, dodgeChance, enragedChance, isEnraged, isPoisoned
+	int enemyData[] = { 1 * ((rand() % level) + level / 2 + 1), 20 * ((rand() % level) + level / 2 + 1), 1 * ((rand() % level) + level / 2 + 1), 25 + ((rand() % level) + level / 2), 0, 0 };
+	int enemyLevel = (enemyData[0] - 1) + (enemyData[1] / 20);
+	if (enemyLevel > 10) {
+		enemyLevel = 11;
+		enemyData[0] *= 3;
+		enemyData[1] *= 2;
+		enemyData[2] *= 5;
+		enemyData[3] *= 2;
+	}
+	if (enemyData[2] > 90) {
+		enemyData[2] = 90;
+	}
+	else if (enemyLevel < 1) {
+		enemyLevel = 1;
+	}
 
-// Sets the games individual variables
-void setValues() {
-	// Assigns the three guessable values
-	a = (rand() % difficulty) + 1;
-	b = (rand() % difficulty) + 1;
-	c = (rand() % difficulty) + 1;
-	// Calculates the hint values
-	addVal = a + b + c;
-	multVal = a * b * c;
-}
+	cout << "A wild " << names[enemyLevel - 1] << " (Lvl: " << enemyLevel << ") approaches!\n" << endl;
 
-// Runs a single instance of the game
-void runGame() {
-	// Re-selects game variables
-	setValues();
+	int playerHealth = playerStartingHealth;
 
-	// Game information outputs
-	cout << "Difficulty level: " << difficulty << endl;
-	cout << "Hint: " << a << " " << b << " " << c << endl;
-	cout << "Numbers adds to " << addVal << " and multiplies to " << multVal << endl;
-
-	// Guess loop variables
-	int guessA = 0, guessB = 0, guessC = 0;
-	bool guessedCorrectly = false;
-	// Loops guesses until the player gets it right
-	while (!guessedCorrectly)
-	{	
-		// Gets player guesses
-		cout << "Value A: ";
-		cin >> guessA;
-		cout << "Value B: ";
-		cin >> guessB;
-		cout << "Value C: ";
-		cin >> guessC;
-		// Confirms if the player is correct
-		if ((guessA * guessB * guessC != multVal) || (guessA + guessB + guessC != addVal)) {
-			cout << "Incorrect!" << endl;
-		} else {
-			guessedCorrectly = true;
+	while (enemyData[1] > 0 && playerHealth > 0) {
+		int action;
+		cout << "Health: " << playerHealth << " | " << names[enemyLevel - 1] << " Health: " << enemyData[1] << endl;
+		cout << "   1: Glare\n   2: Spit\n   3: Heal\n: ";
+		cin >> action;
+		switch (action) {
+		case 1:
+			if (rand() % 100 <= enemyData[2]) {
+				cout << names[enemyLevel - 1] << " dodges" << endl;
+			}
+			else {
+				int damageAmt = 3;
+				enemyData[1] -= damageAmt;
+				cout << names[enemyLevel - 1] << " takes " << damageAmt << " damage" << endl;
+				if (rand() % 100 <= level) {
+					damageAmt *= (rand() % (level + 1));
+					enemyData[1] -= damageAmt;
+					cout << "CRITICAL HIT (x" << 3 / damageAmt << ")" << endl;
+				}
+			}
+			break;
+		case 2:
+			enemyData[5] = 1;
+			cout << names[enemyLevel - 1] << " is now poisoned" << endl;
+			break;
+		case 3:
+			int healthBoostAmt = 3 * level;
+			playerHealth += healthBoostAmt;
+			cout << "+" << healthBoostAmt << " health" << endl;
+			if (rand() % 100 <= enemyData[3]) {
+				cout << names[enemyLevel - 1] << " is now enraged" << endl;
+				enemyData[4] = 3;
+			}
+			break;
 		}
+		do {
+			playerHealth -= enemyData[0];
+			if (enemyLevel == 11) {
+				cout << names[enemyLevel - 1] << " uses annililation, doing " << enemyData[0] << " damage" << endl;
+				if (rand() % 100 <= level * 2) {
+					playerHealth -= enemyData[0];
+					cout << "CRITICAL HIT (x2 damage)" << endl;
+				}
+				else if (rand() % 100 <= level) {
+					playerHealth -= enemyData[0] * 2;
+					cout << "SUPREME CRITICAL HIT (x3 damage)" << endl;
+				}
+			}
+			else {
+				cout << names[enemyLevel - 1] << " does " << enemyData[0] << " damage" << endl;
+				if (rand() % 100 <= level) {
+					playerHealth -= enemyData[0];
+					cout << "CRITICAL HIT (x2 damage)" << endl;
+				}
+			}
+			if (enemyData[5] == 1) {
+				enemyData[1] -= 1 * level;
+				cout << names[enemyLevel - 1] << " take " << 1 * level << " poison damage" << endl;
+				if (rand() % 100 <= 25) {
+					cout << names[enemyLevel - 1] << " recovers from poison" << endl;
+					enemyData[5] = 0;
+
+				}
+			}
+			if (enemyData[4] > 0) {
+				enemyData[4] -= 1;
+			}
+		} while (enemyData[4] > 0);
+	}
+	if (enemyData[1] > playerHealth) {
+		cout << "You died!" << endl;
+		return false;
+	}
+	else if (playerHealth > enemyData[1]) {
+		cout << "You won!" << endl;
+		return true;
+	}
+	else {
+		cout << "Both you and the " << names[enemyLevel - 1] << " have died!" << endl;
+		return false;
 	}
 }
 
-// Manages the interface over multiple games
 int main() {
-	// Sets random seed
 	srand(time(NULL));
-
-	// Continue response variable
-	string continueResponse = "n";
-	
-	// Repeats the game until the player quits
-	do {
-		// Runs the game instance
-		runGame();
-		
-		// Quit option
-		cout << "Congratulations! You've won!\nPlay again? (y/n) ";
-		cin >> continueResponse;
-		difficulty++;
-	} while (continueResponse != "n");
-
-	// End message
-	cout << "Thanks for playing!";
-	return 0;
+	bool playAgain = true;
+	int level = 1;
+	cout << "Welcome player...\n\n";
+	while (playAgain) {
+		if (fight(10 * level, level)) {
+			level++;
+			cout << "Player has levelled up! (Lvl:" << level << ")" << endl;
+			cout << "+10 Maximum Life" << endl;
+		}
+		playAgain = false;
+		string continueResponse;
+		do {
+			cout << "\nContinue? (y/n): ";
+			cin >> continueResponse;
+		} while (!(continueResponse == "y" || continueResponse == "n"));
+		if (continueResponse == "y") {
+			playAgain = true;
+		}
+	}
+	cout << "Safe travels, adventurer!" << endl;
 }

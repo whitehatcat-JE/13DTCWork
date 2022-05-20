63543e3bf965e1dc2a8d6cce97447db8459f15c0
Whitehatcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2022-05-20 18:17:40 +1200

    Added user input for task 6 of prac 103.1

--- Jayden_Everest_103.1-Practical/Task6/Task6/Task6.cpp
+++ Jayden_Everest_103.1-Practical/Task6/Task6/Task6.cpp
@@ -1,20 +1,118 @@
-// Task6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Section B - Task 6 - Jayden Everest
+// Includes
 #include <iostream>
+#include <stdlib.h>
+// Using Declarations
+using std::cout;
+using std::cin;
+using std::endl;
+
+const int DAYS = 7;
+
+struct DailyExpense {
+    int day;
+    int month;
+    int year;
+    float transportCost;
+    float mealCost;
+    float entertainmentCost;
+    float otherCost;
+    DailyExpense(int dayDate, int monthDate, int yearDate, float transport, float meal, float entertain, float other) {
+        day = dayDate;
+        month = monthDate;
+        year = yearDate;
+        transportCost = transport;
+        mealCost = meal;
+        entertainmentCost = entertain;
+        otherCost = other;
+    }
+};
+
+void incrementDate(int* day, int* month, int* year) {
+    *day += 1;
+    switch (*month) {
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        if (*day == 31) {
+            *day = 1;
+            *month += 1;
+            if (*month == 13) {
+                *month = 1;
+                *year += 1;
+            }
+        }
+        break;
+    case 2:
+        if (*year % 4 == 0 && (*year % 100 != 0 || *year % 400 == 0)) {
+            if (*day == 29) {
+                *day = 1;
+                *month = 3;
+            }
+        }
+        else {
+            if (*day == 28) {
+                *day = 1;
+                *month = 3;
+            }
+        }
+        break;
+    case 4: case 6: case 9: case 11:
+        if (*day == 30) {
+            *day = 1;
+            *month += 1;
+        }
+        break;
+    }
+}
+
+void updateExpenses(DailyExpense* recordedExpenses) {
+    int dayDate, monthDate, yearDate;
+    cout << "\n Enter date of first expense:\n\tDay: ";
+    cin >> dayDate;
+    cout << "\tMonth: ";
+    cin >> monthDate;
+    cout << "\tYear: ";
+    cin >> yearDate;
+    cout << "---------------\n";
+    for (int day = 0; day < DAYS; day++) {
+        (*(recordedExpenses + day)).day = dayDate;
+        (*(recordedExpenses + day)).month = monthDate;
+        (*(recordedExpenses + day)).year = yearDate;
+        cout << "\nEnter transport cost for day " << day + 1 << ": ";
+        cin >> (*(recordedExpenses + day)).transportCost;
+        cout << "Enter meal cost for day " << day + 1 << ": ";
+        cin >> (*(recordedExpenses + day)).mealCost;
+        cout << "Enter entertainment cost for day " << day + 1 << ": ";
+        cin >> (*(recordedExpenses + day)).entertainmentCost;
+        cout << "Enter other costs for day " << day + 1 << ": ";
+        cin >> (*(recordedExpenses + day)).otherCost;
+        incrementDate(&dayDate, &monthDate, &yearDate);
+    }
+}
+
+void displayDaily(DailyExpense* recordedExpenses) {
+
+}
+
+void displayWeekly(DailyExpense* recordedExpenses) {
+
+}
+
+void menu(DailyExpense* recordedExpenses) {
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    DailyExpense recordedExpenses[DAYS] = {
+        DailyExpense(1, 1, 2022, 15.9, 45, 0, 0),
+        DailyExpense(2, 1, 2022, 16, 60.5, 0, 2.5),
+        DailyExpense(3, 1, 2022, 14.54, 43.2, 12.5, 0),
+        DailyExpense(4, 1, 2022, 19.32, 76, 0, 0),
+        DailyExpense(5, 1, 2022, 12.1, 32.2, 0, 4.75),
+        DailyExpense(6, 1, 2022, 0, 47.8, 25, 0),
+        DailyExpense(7, 1, 2022, 0, 102.5, 74.5, 3.0)
+    };
+
+    menu(recordedExpenses);
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
